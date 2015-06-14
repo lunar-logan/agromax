cf45ef5885cc610bea5ec0d159e27f6b90dcacde
lunar-logan <mrprogrammar@gmail.com>
2015-06-14 15:43:28 +0530

    ResourceManager class is almost complete

--- src/org/agromax/ResourceManager.java
+++ src/org/agromax/ResourceManager.java
@@ -1,10 +1,13 @@
 package org.agromax;
 
+import com.mongodb.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.nio.file.Paths;
 
 /**
@@ -13,34 +16,77 @@ import java.nio.file.Paths;
 public class ResourceManager {
     private static final ResourceManager ourInstance = new ResourceManager();
 
-    private boolean cacheRefresh = false;
-
     private static final int BUFFER_SIZE = 1024; // In bytes
+
+    private static final String DATABASE_NAME = "agromax";
+
+    private static final String COLLECTION_NAME = "doc_cache";
+
+    private final DBCollection cache;
 
     public static ResourceManager getInstance() {
         return ourInstance;
     }
 
     private ResourceManager() {
+        cache = setupDB();
     }
 
-    public String get(URI theUri) {
+    private DBCollection setupDB() {
+        try {
+            MongoClient mongoClient = new MongoClient();
+            DB db = mongoClient.getDB(DATABASE_NAME);
+            return db.getCollection(COLLECTION_NAME);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String get(URI theUri, boolean cacheRefresh) {
         String buffer = null;
         if (theUri.getScheme().equalsIgnoreCase("file"))
             buffer = retrieveLocal(theUri);
         else
-            buffer = retrieveRemote(theUri);
+            buffer = retrieveRemote(theUri, cacheRefresh);
         return buffer;
     }
 
-    private String retrieveRemote(URI theUri) {
+    public String get(String path) throws URISyntaxException {
+        if (path.matches("^[a-zA-Z]+://(/)?.+")) { // Its a URI
+            return get(new URI(path), false);
+        } else {
+            // Probably a file path, without mentioned scheme "file"
+            return get(Paths.get(path).toUri(), false);
+        }
+    }
+
+    private String retrieveRemote(URI theUri, boolean cacheRefresh) {
+        String uri = theUri.toString();
+
+        BasicDBObject query = new BasicDBObject("uri", uri);
+        DBObject result = cache.findOne(query);
         try {
-            Document soup = Jsoup.connect(theUri.toString()).get();
-            return soup.body().text();
+            String content;
+            if (result == null || cacheRefresh) {
+                Document soup = Jsoup.connect(uri).get();
+                content = soup.body().text();
+
+                BasicDBObject doc = new BasicDBObject("uri", uri).append("content", content);
+                WriteResult writeResult = null;
+
+                if (result == null) writeResult = cache.insert(doc);
+                else if (cacheRefresh) writeResult = cache.update(result, doc);
+                System.out.println("Trying to add: " + doc);
+                System.out.println("Result: " + writeResult);
+
+                return content;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        System.out.println("Cache hit: " + result);
+        return (String) result.get("content");
     }
 
     private String retrieveLocal(URI theUri) {
@@ -61,11 +107,13 @@ public class ResourceManager {
         }
         return null;
     }
-
-    public static void main(String[] args) {
-        ResourceManager rm = ResourceManager.getInstance();
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(rm.get(Paths.get(System.getProperty("user.dir") + "\\.gitignore").toUri()));
-        System.out.println(rm.get(URI.create("http://iitk.ac.in")));
+/*
+    public static void main(String[] args) throws URISyntaxException {
+//        ResourceManager rm = ResourceManager.getInstance();
+//        System.out.println(System.getProperty("user.dir"));
+//        System.out.println(rm.get(Paths.get(System.getProperty("user.dir") + "\\.gitignore").toUri()));
+//        System.out.println(rm.get("http://iitk.ac.in/doaa"));
+//        System.out.println(rm.get(System.getProperty("user.dir") + "\\.gitignore"));
     }
+    */
 }
